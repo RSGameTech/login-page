d972694e806ed5bd43096091b3fdca10fc3643f9
RSGameTech <rsgame0604@gmail.com>
2024-11-02 19:04:23 +0530

    Added funtionality to change Background and added register page :)

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const username = document.querySelector("#username");
 const password = document.querySelector("#password");
 
 const login_msg = document.querySelector(".container");
+const changeBgButton = document.querySelector(".settings button");
+
+const backgrounds = ["assets/bg1.jpg", "assets/bg2.jpg", "assets/bg3.jpg", "assets/bg4.jpg"];
+let currentBgIndex = localStorage.getItem("bgIndex") ? parseInt(localStorage.getItem("bgIndex")) : 0;
+document.body.style.backgroundImage = `url(${backgrounds[currentBgIndex]})`;
 
 login.addEventListener("click", () => {
     if (username.value == "a" && password.value == "a") {
@@ -11,4 +16,10 @@ login.addEventListener("click", () => {
     } else {
         login_msg.innerHTML += "<h3>Incorrect Login Details</h3>";
     }
+});
+
+changeBgButton.addEventListener("click", () => {
+    currentBgIndex = (currentBgIndex + 1) % backgrounds.length;
+    document.body.style.backgroundImage = `url(${backgrounds[currentBgIndex]})`;
+    localStorage.setItem("bgIndex", currentBgIndex);
 });
